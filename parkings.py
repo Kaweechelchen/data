9404cad680045086e0c550dbbb7abca56da1105d
Tezza <thierry@morroni.lu>
2020-02-15 18:45:59 +0100

    new structure

--- parkings.py
+++ parkings.py
@@ -22,20 +22,20 @@ def get_ettelbruck():
         else:
             parking['total'] = int(re.search('(\d+).*', parking_raw['Info1']).group(1))
 
-        free = {}
+        free_detail = {}
         free_count = 0
         for availability in parking_raw['Availabilities']:
             if availability['Type'] == 0:
-                free['general'] = int(availability['Count'])
+                free_detail['general'] = int(availability['Count'])
             elif availability['Type'] == 1:
-                free['disabled'] = int(availability['Count'])
+                free_detail['disabled'] = int(availability['Count'])
             elif availability['Type'] == 2:
-                free['ev'] = int(availability['Count'])
+                free_detail['ev'] = int(availability['Count'])
             else:
                 raise Exception('unknown free parking type [{}]'.format(availability['Type']))
             free_count += int(availability['Count'])
-        free['total'] = int(free_count)
-        parking['free'] = free
+        parking['free'] = int(free_count)
+        parking['free-detail'] = free_detail
         parkings.append(parking)
 
     return parkings
@@ -52,16 +52,20 @@ def get_luxembourg():
         parking = {}
         parking['city'] = 'Luxembourg'
         parking['name'] = parking_raw['title']
-        parking['total'] = parking_raw['vdlxml:total']
-        free = {}
-        if parking_raw['vdlxml:actuel'] is None:
-            free['total'] = None
+        if parking_raw['vdlxml:total'] is None:
+            parking['total'] = None
         else:
-            free['total'] = int(parking_raw['vdlxml:actuel'])
-        free['general'] = None
-        free['disabled'] = None
-        free['ev'] = None
-        parking['free'] = free
+            parking['total'] = int(parking_raw['vdlxml:total'])
+        free_detail = {}
+        if parking_raw['vdlxml:actuel'] is None:
+            parking['free'] = None
+            free_detail['general'] = None
+        else:
+            parking['free'] = int(parking_raw['vdlxml:actuel'])
+            free_detail['general'] = int(parking_raw['vdlxml:actuel'])
+        free_detail['disabled'] = None
+        free_detail['ev'] = None
+        parking['free-detail'] = free_detail
         
         parkings.append(parking)
 

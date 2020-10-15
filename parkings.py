f076225d588b9f41d7c2ade5c5ca4f0f13bea065
Tezza <thierry@morroni.lu>
2020-10-15 11:59:30 +0200

    fix

--- parkings.py
+++ parkings.py
@@ -20,31 +20,35 @@ def get_ettelbruck():
     raw = requests.get(api['ettelbruck']).json()
 
     parkings = []
-    for parking_raw in raw['Parkings']:
-        parking = {}
-        parking['city'] = 'Ettelbruck'
-        parking['name'] = re.search('Parking\s(.*)',parking_raw['Name']).group(1)
-        if parking_raw['Info1'] is None:
-            parking['total'] = None
-        else:
-            parking['total'] = int(re.search('(\d+).*', parking_raw['Info1']).group(1))
-
-        free_detail = {}
-        free_count = 0
-        for availability in parking_raw['Availabilities']:
-            if availability['Type'] == 0:
-                free_detail['general'] = int(availability['Count'])
-            elif availability['Type'] == 1:
-                free_detail['disabled'] = int(availability['Count'])
-            elif availability['Type'] == 2:
-                free_detail['ev'] = int(availability['Count'])
+    try:
+        for parking_raw in raw['Parkings']:
+            parking = {}
+            parking['city'] = 'Ettelbruck'
+            parking['name'] = re.search('Parking\s(.*)',parking_raw['Name']).group(1)
+            if parking_raw['Info1'] is None:
+                parking['total'] = None
             else:
-                raise Exception('unknown free parking type [{}]'.format(availability['Type']))
-            free_count += int(availability['Count'])
-        parking['free'] = int(free_count)
-        if not args.no_details:
-            parking['free-detail'] = free_detail
-        parkings.append(parking)
+                parking['total'] = int(re.search('(\d+).*', parking_raw['Info1']).group(1))
+
+            free_detail = {}
+            free_count = 0
+            for availability in parking_raw['Availabilities']:
+                if availability['Type'] == 0:
+                    free_detail['general'] = int(availability['Count'])
+                elif availability['Type'] == 1:
+                    free_detail['disabled'] = int(availability['Count'])
+                elif availability['Type'] >= 2:
+                    free_detail['ev'] = int(availability['Count'])
+                else:
+                    raise Exception('unknown free parking type [{}]'.format(availability['Type']))
+                free_count += int(availability['Count'])
+            parking['free'] = int(free_count)
+            if not args.no_details:
+                parking['free-detail'] = free_detail
+            parkings.append(parking)
+    except Exception as e:
+        print(e)
+        return []
 
     return parkings
 
@@ -56,27 +60,30 @@ def get_luxembourg():
     )
 
     parkings = []
-    for parking_raw in raw['rss']['channel']['item']:
-        parking = {}
-        parking['city'] = 'Luxembourg'
-        parking['name'] = parking_raw['title']
-        if parking_raw['vdlxml:total'] is None:
-            parking['total'] = None
-        else:
-            parking['total'] = int(parking_raw['vdlxml:total'])
-        free_detail = {}
-        if parking_raw['vdlxml:actuel'] is None:
-            parking['free'] = None
-            free_detail['general'] = None
-        else:
-            parking['free'] = int(parking_raw['vdlxml:actuel'])
-            free_detail['general'] = int(parking_raw['vdlxml:actuel'])
-        free_detail['disabled'] = None
-        free_detail['ev'] = None
-        if not args.no_details:
-            parking['free-detail'] = free_detail
+    try:
+        for parking_raw in raw['rss']['channel']['item']:
+            parking = {}
+            parking['city'] = 'Luxembourg'
+            parking['name'] = parking_raw['title']
+            if parking_raw['vdlxml:total'] is None:
+                parking['total'] = None
+            else:
+                parking['total'] = int(parking_raw['vdlxml:total'])
+            free_detail = {}
+            if parking_raw['vdlxml:actuel'] is None:
+                parking['free'] = None
+                free_detail['general'] = None
+            else:
+                parking['free'] = int(parking_raw['vdlxml:actuel'])
+                free_detail['general'] = int(parking_raw['vdlxml:actuel'])
+            free_detail['disabled'] = None
+            free_detail['ev'] = None
+            if not args.no_details:
+                parking['free-detail'] = free_detail
 
-        parkings.append(parking)
+            parkings.append(parking)
+    except:
+        return []
 
     return parkings
 
